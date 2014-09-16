f7f9ac2b9ebe27e2626fdd415e81f66c0830a221
root <root@dev.jbox-web.fr>
2014-09-16 21:50:05 +0200

    Fix: add repository path in factory

--- spec/factories/repository_git.rb
+++ spec/factories/repository_git.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
 
   factory :repository_git, :class => 'Repository::Git' do |f|
-    f.is_default  false
+    f.is_default     false
+    f.sequence(:url) { |n| "/tmp/git_#{n}" }
   end
 
 end
