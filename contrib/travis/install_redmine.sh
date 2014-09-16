3ae736b6bb52408d347f7da60ac497a965b445c4
root <root@dev.jbox-web.fr>
2014-09-16 18:36:55 +0200

    Add Travis script

---
+++ contrib/travis/install_redmine.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+REDMINE_SOURCE_URL="http://www.redmine.org/releases"
+
+REDMINE_NAME="redmine-${REDMINE_VERSION}"
+REDMINE_PACKAGE="${REDMINE_NAME}.tar.gz"
+REDMINE_URL="${REDMINE_SOURCE_URL}/${REDMINE_PACKAGE}"
+
+PLUGIN_SOURCE="jbox-web"
+PLUGIN_NAME="redmine_jenkins"
+
+CURRENT_DIR=$(pwd)
+
+echo ""
+echo "######################"
+echo "REDMINE INSTALLATION SCRIPT"
+echo ""
+echo "REDMINE_VERSION : ${REDMINE_VERSION}"
+echo "REDMINE_URL     : ${REDMINE_URL}"
+echo "CURRENT_DIR     : ${CURRENT_DIR}"
+echo "PLUGIN_ORIGIN   : ${PLUGIN_SOURCE}/${PLUGIN_NAME}"
+echo ""
+
+echo "#### GET TARBALL"
+wget "${REDMINE_URL}"
+echo "Done !"
+echo ""
+
+echo "#### EXTRACT IT"
+tar xf "${REDMINE_PACKAGE}"
+echo "Done !"
+echo ""
+
+echo "#### MOVE PLUGIN"
+mv "${PLUGIN_SOURCE}/${PLUGIN_NAME}" "${REDMINE_NAME}/plugins"
+rmdir "${PLUGIN_SOURCE}"
+echo "Done !"
+echo ""
+
+echo "#### CREATE SYMLINK"
+ln -s "${REDMINE_NAME}" "redmine"
+echo "Done !"
+echo ""
+
+echo "#### INSTALL DATABASE FILE"
+if [ "$DATABASE_ADAPTER" == "mysql" ] ; then
+  echo "Type : mysql"
+  cp "redmine/plugins/${PLUGIN_NAME}/spec/database_mysql.yml" "redmine/config/database.yml"
+else
+  echo "Type : postgres"
+  cp "redmine/plugins/${PLUGIN_NAME}/spec/database_postgres.yml" "redmine/config/database.yml"
+fi
+
+echo "Done !"
+echo ""
+
+echo "#### INSTALL RSPEC FILE"
+mkdir "redmine/spec"
+cp "redmine/plugins/${PLUGIN_NAME}/spec/root_spec_helper.rb" "redmine/spec/spec_helper.rb"
+echo "Done !"
+echo ""
+
+echo "######################"
+echo "CURRENT DIRECTORY LISTING"
+echo ""
+
+ls -l "${CURRENT_DIR}"
+echo ""
+
+echo "######################"
+echo "REDMINE PLUGIN DIRECTORY LISTING"
+echo ""
+
+ls -l "${REDMINE_NAME}/plugins"
+echo ""
---
+++ spec/root_spec_helper.rb
@@ -0,0 +1,61 @@
+require 'codeclimate-test-reporter'
+
+require 'rubygems'
+require 'rake'
+require 'rails/all'
+
+require 'database_cleaner'
+require 'factory_girl_rails'
+
+require 'simplecov'
+require 'simplecov-rcov'
+
+## Start CodeClimate TestReporter
+CodeClimate::TestReporter.start
+
+## Configure SimpleCov
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter,
+]
+
+SimpleCov.start 'rails' do
+  add_group 'Redmine Jenkins', 'plugins/redmine_jenkins'
+end
+
+## Load Redmine App
+ENV["RAILS_ENV"] = 'test'
+require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
+
+require 'rspec/rails'
+
+## Load FactoryGirls factories
+Dir[Rails.root.join("plugins/*/spec/factories/**/*.rb")].each {|f| require f}
+
+## Configure RSpec
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.color = true
+  config.fail_fast = false
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
