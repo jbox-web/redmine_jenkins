fa0e1526a894d979659cc518d1b765991481f081
root <root@dev.jbox-web.fr>
2015-03-11 00:29:18 +0100

    Update lib loader

--- lib/redmine_jenkins.rb
+++ lib/redmine_jenkins.rb
@@ -1,10 +1,14 @@
-ActionDispatch::Callbacks.to_prepare do
-  require_dependency 'redmine_jenkins/patches/project_patch'
-  require_dependency 'redmine_jenkins/patches/projects_controller_patch'
-  require_dependency 'redmine_jenkins/patches/projects_helper_patch'
-  require_dependency 'redmine_jenkins/patches/query_patch'
+## Redmine Views Hooks
+require 'redmine_jenkins/hooks/add_activity_icon'
 
-  require_dependency 'redmine_jenkins/hooks/add_activity_icon'
+## Set up autoload of patches
+Rails.configuration.to_prepare do
 
-  require_dependency 'redmine_jenkins/extra_loading'
+  ## Redmine Jenkins Libs and Patches
+  rbfiles = Rails.root.join('plugins', 'redmine_jenkins', 'lib', 'redmine_jenkins', '**', '*.rb')
+  Dir.glob(rbfiles).each do |file|
+    # Exclude Redmine Views Hooks from Rails loader to avoid multiple calls to hooks on reload in dev environment.
+    require_dependency file unless File.dirname(file) == Rails.root.join('plugins', 'redmine_jenkins', 'lib', 'redmine_jenkins', 'hooks').to_s
+  end
+
 end
--- lib/redmine_jenkins/extra_loading.rb
+++ lib/redmine_jenkins/extra_loading.rb
@@ -1,14 +1,20 @@
 module RedmineJenkins
   module ExtraLoading
-    # Adds plugin locales if any
-    # YAML translation files should be found under <plugin>/config/locales/
-    ::I18n.load_path += Dir.glob(File.join(Rails.root, 'plugins', 'redmine_jenkins', 'config', 'locales', '**', '*.yml'))
+
+    # Adds plugin locales
+    ::I18n.load_path += Dir.glob(Rails.root.join('plugins', 'redmine_jenkins', 'config', 'locales', '**', '*.yml'))
 
     # Load Forms and Concerns objects
-    services = File.join(Rails.root, 'plugins', 'redmine_jenkins', 'app', 'services')
-
-    if File.directory?(services)
-      ActiveSupport::Dependencies.autoload_paths += [services]
+    [
+      Rails.root.join('plugins', 'redmine_jenkins', 'app', 'services')
+    ].each do |dir|
+      if Dir.exists?(dir)
+        ActiveSupport::Dependencies.autoload_paths += [dir]
+      end
     end
+
+    # HAML gem
+    Haml::Template.options[:attr_wrapper] = '"'
+
   end
 end
