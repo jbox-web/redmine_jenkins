52a1abc3aedb802fece3403b6d8bdfe61fa73faf
root <root@dev.jbox-web.fr>
2015-03-11 01:14:08 +0100

    DRY Project settings controller

--- app/controllers/jenkins_settings_controller.rb
+++ app/controllers/jenkins_settings_controller.rb
@@ -1,54 +1,38 @@
 class JenkinsSettingsController < ApplicationController
   unloadable
 
-  before_filter :find_project
-
-  layout Proc.new { |controller| controller.request.xhr? ? 'popup' : 'base' }
+  # Redmine ApplicationController method
+  before_filter :find_project_by_project_id
+  before_filter :load_jenkins_settings
 
 
   def save
     if !params[:jenkins_setting].nil?
-      if @jenkins_setting.new_record?
-        jenkins_setting = JenkinsSetting.new(params[:jenkins_setting])
-        jenkins_setting.project_id = @project.id
-
-        if jenkins_setting.save
-          flash[:notice] = l(:notice_settings_created)
-        else
-          flash[:error] = jenkins_setting.errors.full_messages.to_sentence
-        end
+      if @jenkins_setting.update_attributes(params[:jenkins_setting])
+        flash[:notice] = l(:notice_settings_updated)
       else
-        if @jenkins_setting.update_attributes(params[:jenkins_setting])
-          flash[:notice] = l(:notice_settings_updated)
-        else
-          flash[:error] = @jenkins_setting.errors.full_messages.to_sentence
-        end
+        flash[:error] = @jenkins_setting.errors.full_messages.to_sentence
       end
     end
-
-    redirect_to :controller => 'projects', :action => 'settings', :tab => 'jenkins', :id => @project
+    redirect_to settings_project_path(@project, 'jenkins')
   end
 
 
   def test_connection
     @content = @jenkins_setting.test_connection
+    render layout: false
   end
 
 
   private
 
 
-  def find_project
-    @project = Project.find(params[:project_id])
-    if @project.nil?
-      render_404
+    def load_jenkins_settings
+      if @project.jenkins_setting.nil?
+        @jenkins_setting = @project.build_jenkins_setting
+      else
+        @jenkins_setting = @project.jenkins_setting
+      end
     end
-
-    if @project.jenkins_setting.nil?
-      @jenkins_setting = JenkinsSetting.new
-    else
-      @jenkins_setting = @project.jenkins_setting
-    end
-  end
 
 end
