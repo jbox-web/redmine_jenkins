665d01151d7f629016942279a6a8bf47de89c81e
root <root@dev.jbox-web.fr>
2015-03-11 02:01:30 +0100

    Indent

--- app/models/jenkins_build.rb
+++ app/models/jenkins_build.rb
@@ -3,7 +3,7 @@ class JenkinsBuild < ActiveRecord::Base
 
   ## Relations
   belongs_to :jenkins_job
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author,     class_name: 'User', foreign_key: 'author_id'
   has_many   :changesets, class_name: 'JenkinsBuildChangeset', dependent: :destroy
 
   ## Validations
