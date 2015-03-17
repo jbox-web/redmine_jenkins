777d5ecdb0b796d9a7b0c70734351210786705f3
root <root@dev.jbox-web.fr>
2015-03-18 00:11:49 +0100

    Update Redmine installer script, test with Redmine latest stable branches

--- contrib/travis/plugin.sh
+++ contrib/travis/plugin.sh
@@ -3,10 +3,11 @@
 GITHUB_USER=${GITHUB_USER:-jbox-web}
 GITHUB_PROJECT=${GITHUB_PROJECT:-redmine_jenkins}
 
-function install_packages() {
+function install_plugin_packages() {
+  log_title "INSTALL ADDITIONAL PACKAGES"
   echo "No package to install"
 }
 
-function install_plugin() {
+function install_plugin_dependencies() {
   git_clone 'redmine_bootstrap_kit' 'https://github.com/jbox-web/redmine_bootstrap_kit.git'
 }
