b6413de7b6b4e806f660ba9794e7fd1b7c46973a
root <root@dev.jbox-web.fr>
2015-03-19 02:31:18 +0100

    First release! Bump to version 1.0.0

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_jenkins do
   name 'Redmine Jenkins plugin'
   author 'Toshiyuki Ando r-labs, Nicolas Rodriguez'
   description 'This is a Jenkins plugin for Redmine'
-  version '1.0-devel'
+  version '1.0.0'
   url 'https://github.com/jbox-web/redmine_jenkins'
   author_url 'https://github.com/jbox-web'
 
